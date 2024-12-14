040c88f6c021543c7339fc967732a3fe2e72db53
Erikson <eriksonribeiro84@gmail.com>
2024-12-14 00:58:30 -0300

    update on project for tasks

--- app/src/main/java/com/example/projectmanager/adapters/TaskListItemAdapter.kt
+++ app/src/main/java/com/example/projectmanager/adapters/TaskListItemAdapter.kt
@@ -2,15 +2,21 @@ package com.example.projectmanager.adapters
 
 import android.content.Context
 import android.content.res.Resources
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager.LayoutParams
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projectmanager.R
+import com.example.projectmanager.activity.TaskListActivity
 import com.example.projectmanager.firebase.FirestoreClass
 import com.example.projectmanager.models.Task
 import org.w3c.dom.Text
@@ -48,20 +54,82 @@ class TaskListItemAdapter(private val context: Context, private var list: ArrayL
             holder.itemView.findViewById<TextView>(R.id.tv_task_list_title).text = model.title
             holder.itemView.findViewById<TextView>(R.id.tv_add_task_list).setOnClickListener {
                 holder.itemView.findViewById<TextView>(R.id.tv_add_task_list).visibility = View.GONE
-                holder.itemView.findViewById<LinearLayout>(R.id.cv_add_task_list_name).visibility = View.VISIBLE
+                holder.itemView.findViewById<CardView>(R.id.cv_add_task_list_name).visibility = View.VISIBLE
             }
 
-            holder.itemView.findViewById<LinearLayout>(R.id.ib_close_list_name).setOnClickListener {
+            holder.itemView.findViewById<ImageButton>(R.id.ib_close_list_name).setOnClickListener {
                 holder.itemView.findViewById<TextView>(R.id.tv_add_task_list).visibility = View.VISIBLE
-                holder.itemView.findViewById<LinearLayout>(R.id.cv_add_task_list_name).visibility = View.GONE
+                holder.itemView.findViewById<CardView>(R.id.cv_add_task_list_name).visibility = View.GONE
             }
 
             holder.itemView.findViewById<ImageButton>(R.id.ib_done_list_name).setOnClickListener {
-                // TODO
+                val listName = holder.itemView.findViewById<EditText>(R.id.et_task_list_name)
+
+
+                if (listName.text.toString().isNotEmpty()) {
+                    if (context is TaskListActivity) {
+                        context.createTaskList(listName.text.toString())
+                    }
+                } else {
+                    Toast.makeText(context, "Please Enter List Name.", Toast.LENGTH_LONG).show()
+                }
             }
 
+            holder.itemView.findViewById<ImageButton>(R.id.ib_edit_list_name).setOnClickListener {
+                holder.itemView.findViewById<EditText>(R.id.et_edit_task_list_name).setText(model.title)
+                holder.itemView.findViewById<LinearLayout>(R.id.ll_title_view).visibility = View.GONE
+                holder.itemView.findViewById<CardView>(R.id.cv_edit_task_list_name).visibility = View.VISIBLE
+            }
+
+            holder.itemView.findViewById<ImageButton>(R.id.ib_close_editable_view).setOnClickListener {
+                holder.itemView.findViewById<LinearLayout>(R.id.ll_title_view).visibility = View.VISIBLE
+                holder.itemView.findViewById<CardView>(R.id.cv_edit_task_list_name).visibility = View.GONE
+            }
+
+            holder.itemView.findViewById<ImageButton>(R.id.ib_done_edit_list_name).setOnClickListener {
+                val listName = holder.itemView.findViewById<EditText>(R.id.et_edit_task_list_name)
+
+                if (listName.text.toString().isNotEmpty()) {
+                    if (context is TaskListActivity) {
+                        context.updateTaskList(position, listName.text.toString(), model)
+                    }
+                } else {
+                    Toast.makeText(context, "Please Enter List Name.", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            holder.itemView.findViewById<ImageButton>(R.id.ib_delete_list).setOnClickListener {
+                alertDialogForDeleteList(position, model.title)
+            }
 
         }
+    }
+
+    private fun alertDialogForDeleteList(position: Int, title: String) {
+        val builder = AlertDialog.Builder(context)
+        //set title for alert dialog
+        builder.setTitle("Alert")
+        //set message for alert dialog
+        builder.setMessage("Are you sure you want to delete $title.")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+        //performing positive action
+        builder.setPositiveButton("Yes") { dialogInterface, which ->
+            dialogInterface.dismiss() // Dialog will be dismissed
+
+            if (context is TaskListActivity) {
+                context.deleteTaskList(position)
+            }
+        }
+
+        //performing negative action
+        builder.setNegativeButton("No") { dialogInterface, which ->
+            dialogInterface.dismiss() // Dialog will be dismissed
+        }
+        // Create the AlertDialog
+        val alertDialog: AlertDialog = builder.create()
+        // Set other dialog properties
+        alertDialog.setCancelable(false) // Will not allow user to cancel after clicking on remaining screen area.
+        alertDialog.show()  // show the dialog to UI
     }
 
 
